dcd90e4271f4f60a6f10db3e06c99e82fe04f176
Antoine Arlaud <antoine@snyk.io>
2018-12-06 17:59:21 -0500

    chore/clean-up

--- scripts/snyk-cli.py
+++ scripts/snyk-cli.py
@@ -7,18 +7,11 @@ import subprocess
 import tarfile
 
 def main(docker_image_to_test):
-
-    # proc = subprocess.Popen(docker_command, shell=True, stdout=subprocess.PIPE)
-    # out, err = proc.communicate()
-    # docker_image_id = out.decode("utf-8").strip('\n')
-
     snyk_token = os.getenv('SNYK_TOKEN')
     snyk_org = os.getenv('SNYK_ORG')
     cfcr_account=os.getenv('CFCR_ACCOUNT')
     cf_user_name=os.getenv('CF_USER_NAME')
     CFCR_LOGIN_TOKEN=os.getenv('CFCR_LOGIN_TOKEN')
-
-
 
     docker_image_name=docker_image_to_test
 
@@ -36,13 +29,8 @@ def main(docker_image_to_test):
 
     proc = subprocess.Popen(docker_command, shell=True)
     out, err = proc.communicate()
-    # docker_image_id = out.decode("utf-8").strip('\n')
-    # print("image")
-    # print(docker_image_id)
-    # print("end of image")
-
+    
     # Base snykcli commnad to scan images
-    #snykcli_base_command = '/usr/local/bin/snyk-linux test https://github.com/aarlaud-snyk/github-stats'
     snykcli_base_command = 'snyk test --docker '
     snykcli_org_option = ''
     if snyk_org:
@@ -57,7 +45,6 @@ def main(docker_image_to_test):
 
     if proc.returncode == 0:
         # Monitor snykcli commnad to scan images
-        #snykcli_monitor_command = '/usr/local/bin/snyk-linux test https://github.com/aarlaud-snyk/github-stats'
         snykcli_monitor_command = 'snyk monitor --docker '
         snykcli_org_option = ''
         if snyk_org:
@@ -70,12 +57,6 @@ def main(docker_image_to_test):
         proc = subprocess.Popen(snykcli_exec, shell=True)
         stdout = proc.communicate()
 
-
-    #
-    #
-    # # Execute command pipe stdout to variable and pipe to stdout and use for final exit code for threshold support
-    # proc = subprocess.Popen(snykcli_exec, shell=True)
-    # proc.communicate()
 
     if proc.returncode != 0:
         sys.exit(1)
